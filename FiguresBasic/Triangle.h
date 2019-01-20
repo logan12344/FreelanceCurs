8ca8b9cd293a919953cd201560eb175a11992d6e
Горбатюк Павел <list12344@gmail.com>
2019-01-20 13:25:15 +0200

    14

--- FiguresBasic/Triangle.h
+++ FiguresBasic/Triangle.h
@@ -12,10 +12,10 @@ public Figure
 public:
     Triangle(double s1=0,double s2=0,double s3=0);
     Triangle(vector<string> pars);
-    virtual double area(void)const;
-    virtual std::string print() const;
-    virtual std::string printToFile() const;
-    virtual std::istream& read(std::istream&);
+    double area(void)const;
+    std::string print() const;
+    std::string printToFile() const;
+    std::istream& read(std::istream&);
 };
 
 #endif /* TRIANGLE_H */
