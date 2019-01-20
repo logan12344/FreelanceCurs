cbfa571e775739947805ce61ff922a721c1d5bf7
Sergey Zolotov <developer2289@devasd.onmicrosoft.com>
2019-01-20 14:33:30 +0200

    25

--- FiguresBasic/FiguresBasic.cpp
+++ FiguresBasic/FiguresBasic.cpp
@@ -91,145 +91,150 @@ int main()
     int *areaValues = nullptr;
     Figure** figures = nullptr;
 
+	int menu = 1;	
 
-	
-
-    std::cout << "Load data from previous session (1 - yes, 2 - no):";
-    //std::cout << "Load data from previous session? \n (type: 0 - to load data in text format, \n 1 - to load data in binary format, \n 2 - don't load previous data):" << std::endl;
-    std::cin >> openFormat;
-    
-    if (openFormat <= 1)
-    {        
-        if (is_file_exist(textDataFile))
-        {
-            std::ifstream infile(textDataFile);
-            string str;
-            vector<Figure*> figrs;
-            vector<string> params;
-            
-            while (infile >> str)//(!str.empty())
-            {
-                if (str == "Circle") {
-                    figrs.push_back(createFigure(eFTCircle, params));
-                    params.clear(); str.clear();
-                }
-                else if (str == "Triangle") {
-                    figrs.push_back(createFigure(eFTTriangle, params));
-                    params.clear(); str.clear();
-                }
-                else if (str == "Rectangle") {
-                    figrs.push_back(createFigure(eFTRectangle, params));
-                    params.clear(); str.clear();
-                }
-                else if (str == "Ellipse") {
-                    figrs.push_back(createFigure(eFTEllipse, params));
-                    params.clear(); str.clear();
-                }
-                else {
-                    params.push_back(str);
-                }
-                //infile >> str;
-            }
-
-            infile.close();
-
-            int cnt = figrs.size();
-            count = cnt;
-            if (cnt > 0)
-            {
-                areaValues = new int[cnt];
-                figures = new Figure*[cnt];
-
-                for (int i = 0; i < cnt; i++)
-                {
-                    figures[i] = figrs[i];
-                }
-            }
-        }
-    }
-    else
-    {
-        std::cout << "Input figures count :  ";
-        std::cin >> count;
-        std::cout << "################################" << std::endl;
-
-        areaValues = new int[count];
-        figures = new Figure*[count];
-
-        try {
-            for (int i = 0; i < count; i++)
-            {
-                int fType = 0;
-                std::cout << "Choose figure type (0 - Circle, 1 - Triangle, 2 - Rectangle, 3 - Ellipse): ";
-                std::cin >> fType;
-
-                //EFiguresTypes t = EFiguresTypes(i % (int)eFTLast);
-                figures[i] = createFigure((EFiguresTypes)fType);
-                std::cout << "================================" << std::endl;
-            }
-        }
-        catch (std::exception e)
-        {
-            std::cout << e.what() << endl;
-        }
-    }
+	while (menu != 0) {
 
 
-    std::cout << std::endl;
-    std::cout << "Print all figures" << std::endl;
-    std::cout << "################################" << std::endl;
+		std::cout << "Load data from previous session (1 - yes, 2 - no):";
+		//std::cout << "Load data from previous session? \n (type: 0 - to load data in text format, \n 1 - to load data in binary format, \n 2 - don't load previous data):" << std::endl;
+		std::cin >> openFormat;
 
-    int sumX = 0;
-    int sumY = 0;
+		if (openFormat <= 1)
+		{
+			if (is_file_exist(textDataFile))
+			{
+				std::ifstream infile(textDataFile);
+				string str;
+				vector<Figure*> figrs;
+				vector<string> params;
 
-    for (int i = 0; i < count; i++)
-    {
-        areaValues[i] = figures[i]->area();
-        sumX += figures[i]->getCenter().x();
-        sumY += figures[i]->getCenter().y();
+				while (infile >> str)//(!str.empty())
+				{
+					if (str == "Circle") {
+						figrs.push_back(createFigure(eFTCircle, params));
+						params.clear(); str.clear();
+					}
+					else if (str == "Triangle") {
+						figrs.push_back(createFigure(eFTTriangle, params));
+						params.clear(); str.clear();
+					}
+					else if (str == "Rectangle") {
+						figrs.push_back(createFigure(eFTRectangle, params));
+						params.clear(); str.clear();
+					}
+					else if (str == "Ellipse") {
+						figrs.push_back(createFigure(eFTEllipse, params));
+						params.clear(); str.clear();
+					}
+					else {
+						params.push_back(str);
+					}
+					//infile >> str;
+				}
 
-        cout << *figures[i] << endl;
-		cout << "\n_______________________________________________________________";
-		cout << "\n|   Area |All figures mean area|All figures center mean (x, y)|";
-		cout << "\n|________|_____________________|______________________________|";
-		cout << "\n|" << setw(8) <<  areaValues[i] << "|" << setw(21) << getArithmeticMean<double>(areaValues, count) << "|" << setw(27) << sumX / count << ", " << sumY / count << "|";
-		cout << "\n|________|_____________________|______________________________|";
-    }
+				infile.close();
 
-    std::cout << "################################" << std::endl;
-    std::cout << "All figures mean area = " << getArithmeticMean<double>(areaValues, count) << std::endl;
-    std::cout << "################################" << std::endl;
-    std::cout << "All figures center mean (x, y) = (" << sumX / count << ", " << sumY / count << ")" << std::endl;
-    std::cout << "################################" << std::endl;
-    std::cout << std::endl;
+				int cnt = figrs.size();
+				count = cnt;
+				if (cnt > 0)
+				{
+					areaValues = new int[cnt];
+					figures = new Figure*[cnt];
 
-    int dataFormat = 0;
-    std::cout << std::endl << "Save data to file..." << std::endl;
-    //std::cout << "Save data to file, choose format (0 - text format, 1 - binary format, 2 - don't save data to file):" << std::endl;
-    //std::cin >> dataFormat;
+					for (int i = 0; i < cnt; i++)
+					{
+						figures[i] = figrs[i];
+					}
+				}
+			}
+		}
+		else
+		{
+			std::cout << "Input figures count :  ";
+			std::cin >> count;
+			std::cout << "################################" << std::endl;
 
-    if (dataFormat <= 1)
-    {
-        ios_base::openmode mode = (dataFormat==0 ? ios_base::out : ios_base::binary);
-        std::ofstream outfile(dataFormat == 0 ? textDataFile : binaryDataFile, mode);
-        //outfile << "Count: " << count << "\n";
-        for (int i = 0; i < count; i++)
-        {
-            //const string &str = figures[i]->print();
-            //outfile.write(str.c_str(), str.size());
-            outfile << figures[i]->printToFile() << "\n";
-        }
-        outfile.close();
-        std::cout << "Data saved" << std::endl;
-    }
-    else
-        std::cout << "Data not Saved" << std::endl;
+			areaValues = new int[count];
+			figures = new Figure*[count];
 
-    std::cout << "-----------------------------------" << std::endl;
+			try {
+				for (int i = 0; i < count; i++)
+				{
+					int fType = 0;
+					std::cout << "Choose figure type (0 - Circle, 1 - Triangle, 2 - Rectangle, 3 - Ellipse): ";
+					std::cin >> fType;
+
+					//EFiguresTypes t = EFiguresTypes(i % (int)eFTLast);
+					figures[i] = createFigure((EFiguresTypes)fType);
+					std::cout << "================================" << std::endl;
+				}
+			}
+			catch (std::exception e)
+			{
+				std::cout << e.what() << endl;
+			}
+		}
 
 
-    delete[] areaValues;
-    delete [] figures;
+		std::cout << std::endl;
+		std::cout << "Print all figures" << std::endl;
+		std::cout << "################################" << std::endl;
+
+		int sumX = 0;
+		int sumY = 0;
+
+		for (int i = 0; i < count; i++)
+		{
+			areaValues[i] = figures[i]->area();
+			sumX += figures[i]->getCenter().x();
+			sumY += figures[i]->getCenter().y();
+
+			cout << *figures[i] << endl;
+			cout << "\n_______________________________________________________________";
+			cout << "\n|   Area |All figures mean area|All figures center mean (x, y)|";
+			cout << "\n|________|_____________________|______________________________|";
+			cout << "\n|" << setw(8) << areaValues[i] << "|" << setw(21) << getArithmeticMean<int>(areaValues, count) << "|" << setw(27) << sumX / count << ", " << sumY / count << "|";
+			cout << "\n|________|_____________________|______________________________|";
+		}
+
+		std::cout << "################################" << std::endl;
+		std::cout << "All figures mean area = " << getArithmeticMean<int>(areaValues, count) << std::endl;
+		std::cout << "################################" << std::endl;
+		std::cout << "All figures center mean (x, y) = (" << sumX / count << ", " << sumY / count << ")" << std::endl;
+		std::cout << "################################" << std::endl;
+		std::cout << std::endl;
+
+		int dataFormat = 0;
+		std::cout << std::endl << "Save data to file..." << std::endl;
+		//std::cout << "Save data to file, choose format (0 - text format, 1 - binary format, 2 - don't save data to file):" << std::endl;
+		//std::cin >> dataFormat;
+
+		if (dataFormat <= 1)
+		{
+			ios_base::openmode mode = (dataFormat == 0 ? ios_base::out : ios_base::binary);
+			std::ofstream outfile(dataFormat == 0 ? textDataFile : binaryDataFile, mode);
+			//outfile << "Count: " << count << "\n";
+			for (int i = 0; i < count; i++)
+			{
+				//const string &str = figures[i]->print();
+				//outfile.write(str.c_str(), str.size());
+				outfile << figures[i]->printToFile() << "\n";
+			}
+			outfile.close();
+			std::cout << "Data saved" << std::endl;
+		}
+		else
+			std::cout << "Data not Saved" << std::endl;
+
+		std::cout << "-----------------------------------" << std::endl;
+
+
+		delete[] areaValues;
+		delete[] figures;
+		cout << "Start - 1, Exit - 0" << endl;
+		cin >> menu;
+	}
     _getch();
 }
 
